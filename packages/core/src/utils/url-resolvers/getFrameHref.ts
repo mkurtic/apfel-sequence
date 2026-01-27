d2619f5d4366b5506dc08b81617743c050b18765
mkurtic <kurtic.mehmed.dev@gmail.com>
2026-01-27 09:35:14 +0100

    fix: encore uri of images path

--- packages/core/src/utils/url-resolvers/getFrameHref.ts
+++ packages/core/src/utils/url-resolvers/getFrameHref.ts
@@ -2,5 +2,5 @@ import type { FrameUrlConfig } from "../../types/utils";
 
 export const getFrameHref = (cfg: FrameUrlConfig, frameNumber: number): string => {
 	const padded = String(frameNumber).padStart(cfg.frameDigits, "0");
-	return cfg.url + (cfg.framePrefix ?? "") + padded + (cfg.frameSuffix ?? "");
+	return encodeURI(cfg.url + (cfg.framePrefix ?? "") + padded + (cfg.frameSuffix ?? ""));
 };
